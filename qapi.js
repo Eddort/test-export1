83dd65ce51122f5d1a1a24ff73dcb8c37b345b0a
Alexander <webisforce@gmail.com>
2020-08-14 12:41:37 +0300

    Some changes

---
+++ qapi.js
@@ -0,0 +1,24 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5f3166ab2fd83b0019f06602",
+				"5f3166ab2fd83b0019f06604"
+			]
+		},
+		"5f3166ab2fd83b0019f06602": {
+			"id": "5f3166ab2fd83b0019f06602",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5f3166ab2fd83b0019f06604": {
+			"id": "5f3166ab2fd83b0019f06604",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"projectType": "gatsby"
+}
